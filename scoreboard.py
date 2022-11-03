8a5058ce04b8970814a8c2c348cad98dd893e372
Satish <satishgummadi4@gmail.com>
2022-11-03 15:09:25 +0530

    Added wall colission detection

--- scoreboard.py
+++ scoreboard.py
@@ -15,6 +15,10 @@ class ScoreBoard(Turtle):
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align='center', font=('Arial', 22, 'normal'))
 
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER!", align='center', font=('Arial', 22, 'normal'))
+
     def increase_score(self):
         self.score += 1
         self.clear()
